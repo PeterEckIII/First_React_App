a5f8e9f1bf0ce4e99980d2cd0f775ecd071f1a1a
Peter Eck <jpeckiii@gmail.com>
2018-08-05 19:57:20 -0500

    Finished basic logic

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   }
 
   nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.find(p => {
+    const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     });
 
@@ -33,7 +33,7 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: person });
+    this.setState({ persons: persons });
   }
 
   deletePersonsHandler = (personIndex) => {
@@ -48,22 +48,15 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
-
     let persons = null;
+    let btnClass = "";
 
     if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
             return <Person
+              click={() => this.deletePersonsHandler(index)}
               name={person.name}
               age={person.age}
               key={person.id}
@@ -71,14 +64,22 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = "red";
+      btnClass = classes.Red;
+    }
+
+    const assignedClasses = [];
+    if(this.state.persons.length <= 2) {
+      assignedClasses.push(classes.red);
+    }
+    if(this.state.persons.length <= 1) {
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>First React App</h1>
         <button
-          style={style}
+          className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
