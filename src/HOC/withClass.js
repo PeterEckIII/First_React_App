c89fe170f7e0483c34d78d5f2381e495fe6d4bf1
Peter Eck <jpeckiii@gmail.com>
2018-08-11 17:01:55 -0500

    Finished HOCs

--- src/HOC/withClass.js
+++ src/HOC/withClass.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+
+// Passing the spread operator with props splits up the props object into separate key / value pairs
+
+// const withClass = (WrappedComponent, className) => {
+//     return(props) => (
+//         <div className={className}>
+//             <WrappedComponent {...props} />
+//         </div>
+//     ) 
+// }
+
+// Above and below are two ways to implement the same idea - one with stateful and one with stateless components
 
 const withClass = (WrappedComponent, className) => {
-    return(props) => (
-        <div className={className}>
-            <WrappedComponent />
-        </div>
-    ) 
+    return class extends Component {
+        render() {
+            return (
+                <div className={className}>
+                    <WrappedComponent {...this.props} />
+                </div>
+            )
+        }
+    }
 }
+
 
 export default withClass;
