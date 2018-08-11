8cabf18e9ea013bc6dc07f2d812e483aaa23ac7e
Peter Eck <jpeckiii@gmail.com>
2018-08-11 16:21:24 -0500

    Working with PureComponent

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
+// Purecomponent essentially includes the shouldComponentUpdate check within itself, helping us out with performance. 
 import Person from "./Person/Person";
 
-class Persons extends Component {
+class Persons extends PureComponent {
     constructor(props) {
         super(props);
         console.log("[Persons.js] Inside Constructor", props)
@@ -19,12 +20,15 @@ class Persons extends Component {
         console.log("[UPDATE Persons.js] inside componentWillReceiveProps", nextProps);
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("[UPDATE Persons.js] inside shouldComponentUpdate", nextProps, nextState);
-        // After removing a person component, if the nextProps.persons is equal to the formerProps (this.props.person) then we want to return false, which means the component shouldn't update
-        // After removing a person component, if the nextProps.persons is NOT equal to the formerProps (as is the case below) then the component SHOULD update.
-        return nextProps.persons !== this.props.persons;
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log("[UPDATE Persons.js] inside shouldComponentUpdate", nextProps, nextState);
+    //     // After removing a person component, if the nextProps.persons is equal to the formerProps (this.props.person) then we want to return false, which means the component shouldn't update
+    //     // After removing a person component, if the nextProps.persons is NOT equal to the formerProps (as is the case below) then the component SHOULD update.
+    //     return nextProps.persons !== this.props.persons || 
+    //     nextProps.changed !== this.props.changed || 
+    //     nextProps.clicked !== this.props.clicked;
+    //     // The above check can help increase performance in the case of several components being on screen. Since we included the 'Show Persons' button in our App.js above the Cockpit component, we don't want the entire lifecycle running when nothing is changing. 
+    // }
 
     componentWillUpdate(nextProps, nextState) {
         console.log("[UPDATE: Persons.js] inside componentWillUpdate", nextProps, nextState);
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from "./App.css";
 import Persons from "../Components/Persons/Persons";
 import Cockpit from "../Components/Cockpit/Cockpit";
 
-class App extends Component {
+class App extends PureComponent {
+  // Only use Purecomponent when 
   constructor(props) {
     super(props);
     console.log("[App.js] Inside Constructor", props)
@@ -29,6 +30,23 @@ class App extends Component {
 
   componentDidMount() {
     console.log("[App.js] inside componentDidMount")
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log("[UPDATE App.js] inside shouldComponentUpdate", nextProps, nextState);
+  //   // After removing a person component, if the nextProps.persons is equal to the formerProps (this.props.person) then we want to return false, which means the component shouldn't update
+  //   // After removing a person component, if the nextProps.persons is NOT equal to the formerProps (as is the case below) then the component SHOULD update.
+  //   return nextProps.persons !== this.props.persons ||
+  //   nextState.showPersons !== this.state.showPersons;
+  //   // return true;
+  // }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log("[UPDATE: App.js] inside componentWillUpdate", nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log("[UPDATE: App.js] inside componentDidUpdate");
   }
   state = {
     persons: [
@@ -86,6 +104,7 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
+        <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           showPersons={this.state.showPersons}
           persons={this.state.persons}
